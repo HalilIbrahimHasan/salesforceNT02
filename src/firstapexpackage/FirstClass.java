45cdc2484605f614679e486799d20cb8c93fb0b7
gurbey61 <gurbey61@gmail.com>
2022-06-01 21:56:14 -0500

    2nd

--- src/firstapexpackage/FirstClass.java
+++ src/firstapexpackage/FirstClass.java
@@ -2,6 +2,13 @@ package firstapexpackage;
 
 public class FirstClass {
 
+    public static void welcome(int n){
+        if(n < 6){
+            System.out.println(n + " Welcome to Java");
+            welcome(n + 1);
+        }
+    }
+
     //welcome everyone on the board!!
     //Hi
 }
